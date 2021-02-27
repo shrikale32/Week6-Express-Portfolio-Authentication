ac2475bd98383884390274758183ea610f431d62
shrikale32 <shrikale32@gmail.com>
2021-02-28 01:36:03 +0530

    connected to MongoDB Atlas and added inline comments

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_list"
+   // "URI": "mongodb://localhost/contact_list"
+
+       "URI" : "mongodb+srv://shrikant:92X7v4wmFEgbuwL8@mongodbserver.gr1rh.mongodb.net/contact-list?retryWrites=true&w=majority"
 }
